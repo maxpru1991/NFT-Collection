62a09eb6ed165a31a1363d40510cefe5aab331bb
maximiliano prudenzano <maximilianoprudenzano@MacBook-Air-de-maximiliano.local>
2022-04-18 21:14:45 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+const WHITELIST_CONTRACT_ADDRESS ="0x77148d6536cae8a3b05f9fb2555d7617e458abb6"
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+
